b1fb8dcd36f6a9ae92052ff6c0c42bb4c1b76f57
chaki <aowp.contact@gmail.com>
2024-09-01 14:25:39 +0900

    cledentialコメントアウト

--- pkg/infra/invitation_infra.go
+++ pkg/infra/invitation_infra.go
@@ -13,10 +13,10 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"log"
 	"time"
-	"github.com/chaki8923/wedding-backend/pkg/lib/config"
+	// "github.com/chaki8923/wedding-backend/pkg/lib/config"
 	"os"
 	"io"
-	"github.com/aws/aws-sdk-go/aws/credentials"
+	// "github.com/aws/aws-sdk-go/aws/credentials"
 )
 
 type invitationRepository struct {
@@ -102,10 +102,10 @@ func (i *invitationRepository) UploadFileToS3(ctx context.Context, file_url grap
 		return "", fmt.Errorf("failed to create temporary file: %w", err)
 	}
 
-	cfg, err := config.New()
-	if err != nil {
-		return "", fmt.Errorf("failed to load config: %w", err)
-	}
+	// cfg, err := config.New()
+	// if err != nil {
+	// 	return "", fmt.Errorf("failed to load config: %w", err)
+	// }
 
 	// ファイルを一時ファイルに書き込む
 	written, err := io.Copy(tempFile, file_url.File)
@@ -123,16 +123,16 @@ func (i *invitationRepository) UploadFileToS3(ctx context.Context, file_url grap
 
 	defer tempFile.Close()
 
-	credential := credentials.NewStaticCredentials(
-		cfg.AwsAccessKey,
-		cfg.AwsSecretKey,
-		"",
-	)
+	// credential := credentials.NewStaticCredentials(
+	// 	cfg.AwsAccessKey,
+	// 	cfg.AwsSecretKey,
+	// 	"",
+	// )
 
 	// ファイルを S3 にアップロードする処理
 	sess := session.Must(session.NewSession(&aws.Config{
 		Region:      aws.String("ap-northeast-1"),
-		Credentials: credential,
+		// Credentials: credential,
 	}))
 	uploader := s3.New(sess)
 
